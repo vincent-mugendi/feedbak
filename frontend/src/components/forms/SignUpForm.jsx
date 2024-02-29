b07ad414e743dd94b08da1c8edd919389f02ff0f
vincent-mugendi <vincentmwathi5@gmail.com>
2024-02-29 23:44:42 +0300

    Updates to the create user api

--- frontend/src/components/forms/SignUpForm.jsx
+++ frontend/src/components/forms/SignUpForm.jsx
@@ -140,6 +140,7 @@ const SignUpForm = () => {
           value={formData.confirmPassword}
           onChange={handleChange}
           required
+          
         />
       </div>
       <button type="submit" disabled={loading}>
